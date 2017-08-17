f892eb14b0bd8f23e25bea0ea3d97051e2147fe2
Chris Mitchelmore <chrismitchelmore@gmail.com>
2017-08-17 14:27:15 +0100

    Cleanup package

--- Package.swift
+++ Package.swift
@@ -7,9 +7,7 @@ let package = Package(
     name: "ECurve",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "ECurve",
-            targets: ["ECurve"]),
+        .library( name: "ECurve", targets: ["ECurve"]),
     ],
     dependencies: [
        .package(url: "https://github.com/crmitchelmore/UInt256.git", from: "1.0.0"),
@@ -17,11 +15,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "ECurve",
-            dependencies: ["UInt256"]),
-        .testTarget(
-            name: "ECurveTests",
-            dependencies: ["ECurve"]),
+        .target( name: "ECurve", dependencies: ["UInt256"]),
+        .testTarget(name: "ECurveTests", dependencies: ["ECurve"]),
     ]
 )
